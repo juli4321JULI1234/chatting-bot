b677f33c1fdaccacdd8f5d0ed967de486000782e
juli1234JULI4321 <h311113@gmail.com>
2019-12-10 22:07:41 +0800

    Eat!

--- fsm.py
+++ fsm.py
@@ -64,7 +64,7 @@ class TocMachine(GraphMachine):
         print("I'm entering state2")
 
         reply_token = event.reply_token
-        send_text_message(reply_token, "選擇飲料嗎?(是/否)")
+        send_text_message(reply_token, "選擇飲料嗎?(是否)")
 
     def on_exit_state2(self, event):
         print("Leaving state2")
@@ -73,7 +73,7 @@ class TocMachine(GraphMachine):
         print("I'm entering state3")
 
         reply_token = event.reply_token
-        send_text_message(reply_token, "選擇主菜嗎?(是/否)")
+        send_text_message(reply_token, "選擇主菜嗎?(是否)")
 
     def on_exit_state3(self, event):
         print("Leaving state3")
@@ -82,7 +82,7 @@ class TocMachine(GraphMachine):
         print("I'm entering state4")
 
         reply_token = event.reply_token
-        send_text_message(reply_token, "選擇甜點嗎?(是/否)")
+        send_text_message(reply_token, "選擇甜點嗎?(是否)")
 
     def on_exit_state4(self, event):
         print("Leaving state4")
