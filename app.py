33b722e4f196ba874b62351bc12d093d9bf249e0
juli1234JULI4321 <h311113@gmail.com>
2019-12-10 21:57:19 +0800

    Eat!

--- app.py
+++ app.py
@@ -90,8 +90,6 @@ machine = TocMachine(
 
 app = Flask(__name__, static_url_path="")
 
-#ssss
-
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
@@ -111,7 +109,7 @@ def callback():
     signature = request.headers["X-Line-Signature"]
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    #app.logger.info("Request body: " + body)
     
     # parse webhook body
     try:
@@ -138,7 +136,7 @@ def webhook_handler():
     signature = request.headers["X-Line-Signature"]
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info(f"Request body: {body}")
+   # app.logger.info(f"Request body: {body}")
 
     # parse webhook body
     try:
